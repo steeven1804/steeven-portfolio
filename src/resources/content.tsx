606f136ae00900143efbaf99968436a048479230
steeven1804 <steevenyanez11@gmail.com>
2025-12-30 10:15:52 -0500

    confirmacion

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,14 +2,14 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "Steeven",
+  lastName: "Yanez",
+  name: `Steeven Yanez`,
+  role: "Desarrolador Full Stack",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "steevenyanez11@gmail.com",
+  location: "Ambato/Ecuador", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["Español", "Ecuador"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
